e0a95451ed674796424c2d4ac7a0994574a5b7fa
Marcos Azevedo <marcosviniciuspinheiro63@gmail.com>
2025-10-31 23:52:30 -0300

    implementação da função que gera pacotes e da thread de leitura do teclado

--- TP_ATR/Processo_principal.cpp
+++ TP_ATR/Processo_principal.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <process.h>    // _beginthreadex() e _endthreadex()
 #include <conio.h>      // _getch
+#include <cstdlib>      // rand_s
 
 #define TAMANHO_LISTA_1 200
 
@@ -29,12 +30,84 @@ void InicializarLista(ListaCircular& lista, int tamanho);
 DWORD WINAPI TarefaLeituraMedicao(LPVOID lpParam);
 DWORD WINAPI TarefaLeituraCLP(LPVOID lpParam);
 DWORD WINAPI TarefaRetiradaMensagens(LPVOID lpParam);
+DWORD WINAPI TarefaLeituraTeclado(LPVOID lpParam);
 
 // --- Variáveis Globais ---
 ListaCircular lista1;
-HANDLE hThreads[3]; // 0: Medicao, 1: CLP, 2: Retirada
+HANDLE hThreads[4]; // 0: Medicao, 1: CLP, 2: Retirada, 3: Teclado
 HANDLE hEventoTermino;
-HANDLE hEventoBloqueioMedicao, hEventoBloqueioCLP, hEventoBloqueioRetirada;
+HANDLE hEventoBloqueioMedicao, hEventoBloqueioCLP, hEventoBloqueioRetirada, hEventoESC;
+
+void GerarDados(int tipo, int nseq, char* VariavelRetorno)
+{ // de acordo com minhas fontes, é recomendado usar rand_s, já q é aplicação multithread. Só que tava dando mto erro de compilação.
+    //como a função é chamada entre mutexes, talvez n de problema
+    if (tipo == 11)
+    {
+        // Gera os dados 
+
+		int id_disco = (rand() % 2) + 1;
+
+        float gr_med = (float)(rand() % 10001) / 100.0f;
+        
+        float gr_max = (float)(rand() % 10001) / 100.0f;
+
+        float gr_min = (float)(rand() % 10001) / 100.0f;
+
+        float sigma = (float)(rand() % 10001) / 100.0f;
+
+		// Obtém o timestamp
+		char timestamp[9];
+		SYSTEMTIME st;
+		GetLocalTime(&st);
+		sprintf(timestamp, "%02d:%02d:%02d", st.wHour, st.wMinute, st.wSecond);
+
+        sprintf(lista1.buffer[lista1.tail],
+            "11/%04ld/%02d/%s/%06.2f/%06.2f/%06.2f/%06.2f",
+            nseq,
+            id_disco,
+            timestamp,
+            gr_med,
+            gr_max,
+            gr_min,
+            sigma
+        );
+    }
+    if (tipo == 44)
+    {
+        // Gera os dados
+        float vel, incl, pot, vz_ent, vz_saida;
+        int id_disco = (rand() % 6) + 1;
+
+        vel = (float)(rand() % 10001) / 10.0f;
+ 
+        incl = (float)(rand() % 451) / 10.0f;
+       
+        pot = (float)(rand() % 2001) / 1000.0f;
+        
+        vz_ent = (float)(rand() % 10001) / 10.0f;
+        
+        vz_saida = (float)(rand() % 10001) / 10.0f;
+
+		//obtém o timestamp
+        char timeStr[13]; // HH:MM:SS:MMM + \0
+        SYSTEMTIME st;
+        GetLocalTime(&st);
+        sprintf(timeStr, "%02d:%02d:%02d:%03d",
+            st.wHour, st.wMinute, st.wSecond, st.wMilliseconds); // esse timestamp aq n tá mostrando os milissegundos
+
+        sprintf(VariavelRetorno,
+            "44/%04d/%02d/%s/%06.1f/%04.1f/%05.3f/%06.1f/%06.1f", 
+            nseq,
+            id_disco,
+            timeStr,
+            vel,
+            incl,
+            pot,
+            vz_ent,
+            vz_saida
+        );
+    }
+}
 
 
 int main() {
@@ -54,6 +127,7 @@ int main() {
     hEventoBloqueioMedicao = CreateEvent(NULL, FALSE, FALSE, NULL);
     hEventoBloqueioCLP = CreateEvent(NULL, FALSE, FALSE, NULL);
     hEventoBloqueioRetirada = CreateEvent(NULL, FALSE, FALSE, NULL);
+	hEventoESC = CreateEvent(NULL, FALSE, FALSE, NULL);
 
     // Etapa 4: Criar as threads de trabalho
     printf("Criando threads de trabalho...\n");
@@ -66,41 +140,46 @@ int main() {
     hThreads[2] = (HANDLE)_beginthreadex(NULL, 0, (CAST_FUNCTION)TarefaRetiradaMensagens, NULL, 0, (CAST_LPDWORD)&dwThreadId);
     if (hThreads[2]) printf("Thread de Retirada criada com Id = %0x\n", dwThreadId);
 
+    hThreads[3] = (HANDLE)_beginthreadex(NULL, 0, (CAST_FUNCTION)TarefaLeituraTeclado, NULL, 0, (CAST_LPDWORD)&dwThreadId);
+    if (hThreads[3]) printf("Thread de Leitura do teclado criada com Id = %0x\n", dwThreadId);
+
     printf("\n--- Sistema em operacao ---\n");
     printf("Pressione 'm', 'p', 'r' para pausar/retomar as tarefas.\n");
     printf("Pressione ESC para finalizar.\n");
 
     // Etapa 5: Loop de leitura do teclado
-    int tecla;
-    do {
-        tecla = _getch();
-        switch (tecla) {
-        case 'm': case 'M':
-            printf("[Controle] Comando para bloquear/desbloquear Medicao recebido.\n");
-            SetEvent(hEventoBloqueioMedicao);
-            break;
-        case 'p': case 'P':
-            printf("[Controle] Comando para bloquear/desbloquear CLP recebido.\n");
-            SetEvent(hEventoBloqueioCLP);
-            break;
-        case 'r': case 'R':
-            printf("[Controle] Comando para bloquear/desbloquear Retirada recebido.\n");
-            SetEvent(hEventoBloqueioRetirada);
-            break;
-        }
-    } while (tecla != 27); // ESC
+    //int tecla;
+    //do {
+    //    tecla = _getch();
+    //    switch (tecla) {
+    //    case 'm': case 'M':
+    //        printf("[Controle] Comando para bloquear/desbloquear Medicao recebido.\n");
+    //        SetEvent(hEventoBloqueioMedicao);
+    //        break;
+    //    case 'p': case 'P':
+    //        printf("[Controle] Comando para bloquear/desbloquear CLP recebido.\n");
+    //        SetEvent(hEventoBloqueioCLP);
+    //        break;
+    //    case 'r': case 'R':
+    //        printf("[Controle] Comando para bloquear/desbloquear Retirada recebido.\n");
+    //        SetEvent(hEventoBloqueioRetirada);
+    //        break;
+    //    }
+    //} while (tecla != 27); // ESC
 
-    // Etapa 6: Sinalizar o término e aguardar as threads
+    // Etapa 5: Sinalizar o término e aguardar as threads
+	WaitForSingleObject(hEventoESC, INFINITE);
     printf("\nTecla ESC pressionada. Notificando threads para terminarem...\n");
     SetEvent(hEventoTermino);
 
-    WaitForMultipleObjects(3, hThreads, TRUE, INFINITE);
+    WaitForMultipleObjects(4, hThreads, TRUE, INFINITE);
     printf("Todas as threads de trabalho terminaram.\n");
 
-    // Etapa 7: Limpeza dos handles
+    // Etapa 6: Limpeza dos handles
     CloseHandle(hThreads[0]);
     CloseHandle(hThreads[1]);
     CloseHandle(hThreads[2]);
+	CloseHandle(hThreads[3]);
     CloseHandle(hEventoTermino);
     CloseHandle(hEventoBloqueioMedicao);
     CloseHandle(hEventoBloqueioCLP);
@@ -142,8 +221,9 @@ DWORD WINAPI TarefaLeituraMedicao(LPVOID lpParam) {
         // --- TRABALHO DO PRODUTOR ---
         WaitForSingleObject(lista1.hSemaforoVazios, INFINITE);
         WaitForSingleObject(lista1.hMutex, INFINITE);
-
         sprintf(lista1.buffer[lista1.tail], "MSG_MEDICAO_%d", contadorMsg++);
+        //GerarDados(11, contadorMsg, lista1.buffer[lista1.tail]);
+		//contadorMsg++;
         printf("[Medicao] Depositou: %s\n", lista1.buffer[lista1.tail]);
         lista1.tail = (lista1.tail + 1) % TAMANHO_LISTA_1;
 
@@ -184,6 +264,9 @@ DWORD WINAPI TarefaLeituraCLP(LPVOID lpParam) {
 
         sprintf(lista1.buffer[lista1.tail], "MSG_CLP_%d", contadorMsg++);
         printf("[CLP] Depositou: %s\n", lista1.buffer[lista1.tail]);
+  //      GerarDados(44, contadorMsg, lista1.buffer[lista1.tail]);
+		//printf("[CLP] Depositou: %s\n", lista1.buffer[lista1.tail]);
+		//contadorMsg++;
         lista1.tail = (lista1.tail + 1) % TAMANHO_LISTA_1;
 
         ReleaseMutex(lista1.hMutex);
@@ -230,6 +313,38 @@ DWORD WINAPI TarefaRetiradaMensagens(LPVOID lpParam) {
     }
     _endthreadex(0);
     return 0;
+}
+
+DWORD WINAPI TarefaLeituraTeclado(LPVOID lpParam) {
+
+    int tecla;
+    while ((true)) {
+        while (_kbhit()) {
+            _getch();
+        }
+        tecla = _getch();
+        switch (tecla) {
+        case 'm': case 'M':
+            printf("[Controle] Comando para bloquear/desbloquear Medicao recebido.\n");
+            SetEvent(hEventoBloqueioMedicao);
+            break;
+        case 'p': case 'P':
+            printf("[Controle] Comando para bloquear/desbloquear CLP recebido.\n");
+            SetEvent(hEventoBloqueioCLP);
+            break;
+        case 'r': case 'R':
+            printf("[Controle] Comando para bloquear/desbloquear Retirada recebido.\n");
+            SetEvent(hEventoBloqueioRetirada);
+            break;
+        }
+
+        if (tecla == 27) { // ESC
+			// Sinaliza o termino
+            printf("\nTecla ESC pressionada. Notificando threads para terminarem...\n");
+            SetEvent(hEventoTermino);
+            return 0;
+        }
+    }
 }
 
 void IniciarProcesso(const char* nomeProcesso) {
